7cc69a3a1f44af0ead2a90656976a02fcad06a32
Sylvain Rebaud <sylvain@rebaud.com>
2016-06-05 22:17:42 -0700

    Fix indentation

--- ReactiveTwitterSearch/TwitterSearchViewController.swift
+++ ReactiveTwitterSearch/TwitterSearchViewController.swift
@@ -11,26 +11,29 @@ import ReactiveCocoa
 
 class TwitterSearchViewController: UIViewController {
 
-  @IBOutlet weak var searchTextField: UITextField!
-  @IBOutlet weak var searchAcitivyIndicator: UIActivityIndicatorView!
-  @IBOutlet weak var executionTimeTextField: UILabel!
-  @IBOutlet weak var tweetsTable: UITableView!
-  
-  private var bindingHelper: TableViewBindingHelper<TweetViewModel>!
-  
-  var viewModel: TwitterSearchViewModel = {
-    let searchService = TwitterSearchService()
-    return TwitterSearchViewModel(searchService: searchService)
-  }()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    viewModel.searchText <~ searchTextField.rac_text
-    searchAcitivyIndicator.rac_hidden <~ viewModel.isSearching.producer.map { !$0 }
-    executionTimeTextField.rac_text  <~ viewModel.queryExecutionTime
-    tweetsTable.rac_alpha <~ viewModel.loadingAlpha.producer.map { $0 }
-    
-    bindingHelper = TableViewBindingHelper(tableView: tweetsTable, sourceSignal: viewModel.tweets.producer, nibName: "TweetCell", selectionCommand: nil)
-  }
+	@IBOutlet weak var searchTextField: UITextField!
+	@IBOutlet weak var searchActivityIndicator: UIActivityIndicatorView!
+	@IBOutlet weak var executionTimeTextField: UILabel!
+	@IBOutlet weak var tweetsTable: UITableView!
+
+	private var bindingHelper: TableViewBindingHelper<TweetViewModel>!
+
+	var viewModel: TwitterSearchViewModel = {
+		let searchService = TwitterSearchService()
+		return TwitterSearchViewModel(searchService: searchService)
+	}()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		viewModel.searchText <~ searchTextField.rac_text
+		searchActivityIndicator.rac_hidden <~ viewModel.isSearching.producer.map { !$0 }
+		executionTimeTextField.rac_text  <~ viewModel.queryExecutionTime
+		tweetsTable.rac_alpha <~ viewModel.loadingAlpha
+
+//		DynamicProperty(object: executionTimeTextField, keyPath: "text") <~ viewModel.queryExecutionTime.producer.map { $0 }
+//		DynamicProperty(object: tweetsTable, keyPath: "alpha") <~ viewModel.loadingAlpha.producer.map { $0 }
+
+		bindingHelper = TableViewBindingHelper(tableView: tweetsTable, sourceSignal: viewModel.tweets.producer, nibName: "TweetCell", selectionCommand: nil)
+	}
 }
--- ReactiveTwitterSearch/ViewModel/TwitterSearchViewModel.swift
+++ ReactiveTwitterSearch/ViewModel/TwitterSearchViewModel.swift
@@ -11,52 +11,58 @@ import ReactiveCocoa
 
 class TwitterSearchViewModel {
 
-  struct Constants {
-    static let SearchCharacterMinimum: Int = 3
-    static let SearchThrottleTime: NSTimeInterval = 1.0
-    static let TickIntervalLength: NSTimeInterval = 1.0
-    static let DisabledTableAlpha: CGFloat = 0.5
-    static let EnabledTableAlpha: CGFloat = 1.0
-  }
-  
-  let searchText = MutableProperty<String>("")
-  let queryExecutionTime = MutableProperty<String>("")
-  let isSearching = MutableProperty<Bool>(false)
-  let tweets = MutableProperty<[TweetViewModel]>([TweetViewModel]())
-  let loadingAlpha = MutableProperty<CGFloat>(Constants.DisabledTableAlpha)
-  
-  private let searchService: TwitterSearchService
+	struct Constants {
+		static let SearchCharacterMinimum: Int = 3
+		static let SearchThrottleTime: NSTimeInterval = 1.0
+		static let TickIntervalLength: NSTimeInterval = 1.0
+		static let DisabledTableAlpha: CGFloat = 0.5
+		static let EnabledTableAlpha: CGFloat = 1.0
+	}
 
-  init(searchService: TwitterSearchService) {
-    
-    self.searchService = searchService
-    
-    searchService.requestAccessToTwitterSignal()
-        .then(searchText.producer.mapError({ _ in TwitterInstantError.NoError.toError() })
-            .filter {
-                $0.characters.count > Constants.SearchCharacterMinimum
-            }
-            .throttle(Constants.SearchThrottleTime, onScheduler: QueueScheduler.mainQueueScheduler)
-            .on(next: {
-                _ in self.isSearching.value = true
-            })
-            .flatMap(.Latest) { text in
-                self.searchService.signalForSearchWithText(text)
-            })
-        .observeOn(QueueScheduler.mainQueueScheduler).startWithNext { response in
-            self.isSearching.value = false
-            self.queryExecutionTime.value = "Execution time: \(response.responseTime)"
-            self.tweets.value = (response.tweets.map { TweetViewModel(tweet: $0) })
-        }
+	let searchText = MutableProperty<String>("")
+	let queryExecutionTime = MutableProperty<String>("")
+	let isSearching = MutableProperty<Bool>(false)
+	let tweets = MutableProperty<[TweetViewModel]>([TweetViewModel]())
+	let loadingAlpha = MutableProperty<CGFloat>(Constants.DisabledTableAlpha)
 
-    timer(Constants.TickIntervalLength, onScheduler: QueueScheduler.mainQueueScheduler).startWithNext { [weak self] _ in
-      self?.tweets.value.forEach { $0.updateTime() }
-    }
+	private let searchService: TwitterSearchService
 
-    loadingAlpha <~ isSearching.producer.map(enabledAlpha)
-  }
+	init(searchService: TwitterSearchService) {
 
-  private func enabledAlpha(searching: Bool) -> CGFloat {
-    return searching ? Constants.DisabledTableAlpha : Constants.EnabledTableAlpha
-  }
+		self.searchService = searchService
+
+		searchService.requestAccessToTwitterSignal()
+			.then(searchText.producer
+				.flatMapError { _ in return SignalProducer.empty }
+				.filter {
+					$0.characters.count > Constants.SearchCharacterMinimum
+				}
+				.throttle(Constants.SearchThrottleTime, onScheduler: QueueScheduler.mainQueueScheduler)
+				.on(next: { [weak self] _ in
+					self?.isSearching.value = true
+				})
+				.flatMap(.Latest) { [weak self] text in
+					(self?.searchService.signalForSearchWithText(text))!
+						.flatMapError { _ in
+							return SignalProducer.empty
+						}
+				})
+			.observeOn(QueueScheduler.mainQueueScheduler)
+			.startWithNext { [weak self] response in
+				self?.isSearching.value = false
+				self?.queryExecutionTime.value = "Execution time: \(response.responseTime)"
+				self?.tweets.value = (response.tweets.map { TweetViewModel(tweet: $0) })
+			}
+
+		timer(Constants.TickIntervalLength, onScheduler: QueueScheduler.mainQueueScheduler)
+			.startWithNext { [weak self] _ in
+				self?.tweets.value.forEach { $0.updateTime() }
+			}
+
+		loadingAlpha <~ isSearching.producer.map(enabledAlpha)
+	}
+
+	private func enabledAlpha(searching: Bool) -> CGFloat {
+		return searching ? Constants.DisabledTableAlpha : Constants.EnabledTableAlpha
+	}
 }
